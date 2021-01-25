b73ce8dc562377e4683df059b2c27c25dd3632d6
Iván <me@ivancr.com>
2021-01-24 21:24:34 -0500

    Mock Data placeholder

---
+++ iOS14UIKitTour/iOS14UIKitTour/Models/MockData.swift
@@ -0,0 +1,46 @@
+import Foundation
+
+enum MockData {
+    static let mockRepos: [Repository] = [
+        .init(name: "WWDCNotes",
+              id: UUID().uuidString,
+              contributors: [])
+
+            .init(name: "NSPersistentCloudKitContainer",
+                  id: UUID().uuidString,
+                  contributors: [])
+
+            .init(name: "challenges",
+                  id: UUID().uuidString,
+                  contributors: [])
+
+            .init(name: "Concurrency101",
+                  id: UUID().uuidString,
+                  contributors: [])
+
+            .init(name: "UICollectionViewTour",
+                  id: UUID().uuidString,
+                  contributors: [])
+            .init(name: "algorithms-club",
+                  id: UUID().uuidString,
+                  contributors: [])
+            .init(name: "Speech-Recognition",
+                  id: UUID().uuidString,
+                  contributors: [])
+            .init(name: "swift-miami-tv",
+                  id: UUID().uuidString,
+                  contributors: [])
+            .init(name: "BackToBasicsUIKit",
+                  id: UUID().uuidString,
+                  contributors: [])
+
+            .init(name: "BackToBasicsUIKit",
+                  id: UUID().uuidString,
+                  contributors: [])
+
+        Data-Structures-and-Algorithms
+        intro-to-unit-tests
+
+    ]
+
+}
